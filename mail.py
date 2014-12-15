d053840f12477be0b322a8edf58018b6d91fd7ef
Sebbs <dongmaster@users.noreply.github.com>
2014-12-15 16:13:20 +0100

    Working on cleaner mail delivery

--- mail.py
+++ mail.py
@@ -1,17 +1,29 @@
+#!/usr/bin/python3
+
 from volapi import Room
 import time
+import json
+import re
 
 # Options
 name = "MailBot"
 room = "PHS-Nb"
 password = "password"
+commands = [
+	"!w",
+	"!m",
+	"!mail"
+]
 
 blacklist = [
 	"anon3000"
 ]
 
-# Don't touch the code below.
+# Don't touch the code below unless you know what you are doing.
 myoc = Room(room, name)
+file_uploader = Room(room, name)
+
+
 
 try:
 	myoc.user.login(password)
@@ -33,15 +45,15 @@ messages = {
 	
 }
 
-is_staff = False
 should_send = True
+should_write = True
 can_split = True
 blacklisted_user = False
 
 def onmessage(msg):
 	global messages
-	global is_staff
 	global should_send
+	global should_write
 	global can_split
 	global blacklisted_user
 	
@@ -58,8 +70,7 @@ def onmessage(msg):
 			print("Could not split message, probably too short.")
 			can_split = False
 		
-		#if splitmsg[0] == "!w" or splitmsg[0] == "!whisper" or splitmsg[0] == "!m" or splitmsg[0] == "!mail":
-		if splitmsg[0] in ("!w", "!whisper", "!m", "!mail") and len(splitmsg) >= 3 and can_split == True:
+		if splitmsg[0] in commands and len(splitmsg) >= 3 and can_split == True:
 			if splitmsg[1].lower() in blacklist:
 				blacklisted_user = True
 				myoc.post_chat("That is a blacklisted user. Sending mail to that person is not allowed.")
@@ -70,19 +81,43 @@ def onmessage(msg):
 				sender = msg.nick
 				message = splitmsg[2]
 				
+				try:
+					reciever = reciever.replace("/", "")
+					reciever = reciever.replace("\\", "")
+					print("Safed the filename, i think")
+				except Exception:
+					print("Couldn't safe filename. Probably a real error or false positive i dunno lol")
 				
-				messages[reciever.casefold()] = {
-					'reciever' : reciever,
-					'sender' : sender,
-					'message' : message
-				}
-				
-				print("got message \n reciever: " + reciever + "\n sender: " + sender + "\n message: " + message)
-				myoc.post_chat("Added to mail pool.")
-				
-				if msg.nick.casefold() == sender.casefold():
-					should_send = False
-	
+				print(str(reciever))
+				if should_write == True and True == False:
+					text_to_write = str("\n \n From: " + sender + "\n To: " + reciever + "\n Message: \n " + message)
+					
+					mail_file = open(reciever.casefold(), "ab")
+					
+					mail_file.write(bytes(text_to_write, 'UTF-8'))
+					mail_file.close()
+					
+					if msg.nick.casefold() in messages == False:
+						messages[reciever.casefold()] = {
+							'filename' : reciever.casefold()
+						}
+					
+					'''
+					messages[reciever.casefold()] = {
+						'reciever' : reciever,
+						'sender' : sender,
+						'message' : message
+					}
+					'''
+					
+					print("got message \n reciever: " + reciever + "\n sender: " + sender + "\n message: " + message)
+					myoc.post_chat("Added to mail pool.")
+					
+					if msg.nick.casefold() == sender.casefold():
+						should_send = False
+				elif should_write == False:
+					myoc.post_chat("Invalid name.")
+	'''
 	try:
 		if should_send == True:
 			dict = messages[msg.nick.casefold()]
@@ -96,7 +131,19 @@ def onmessage(msg):
 	except Exception:
 		print("Failed to deliver mail.")
 		
+	'''
+	'''
+	try:
+		if should_send == True and msg.nick.casefold() in messages:
+		
+			
+			
+			
+	except Exception:
+		print("Failed to deliver mail.")
+		'''
 	should_send = True
+	should_write = True
 	can_split = True
 	blacklisted_user = False
 
